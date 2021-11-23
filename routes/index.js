c74ac93ccc897435481497980627d5fe7dbb15ff
Richard Widjaya <ricwidjaya@gmail.com>
2021-11-23 18:23:56 +0800

    feat: add users module

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const router = express.Router()
 // Import routers
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 // Define route module (index -> Main Router)
 // Landing page
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
 
 // Export route module
 module.exports = router
