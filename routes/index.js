65a2fbae5fbda7d000b12f276bea2a4140308076
Richard Widjaya <ricwidjaya@gmail.com>
2021-12-17 18:49:42 +0800

    refactor: modify login routes

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
 // Import express to use Router
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
 // Import routers
-const home = require('./modules/home')
-const todos = require('./modules/todos')
-const users = require('./modules/users')
+const home = require("./modules/home")
+const todos = require("./modules/todos")
 
 // Define route module (index -> Main Router)
 // Landing page
-router.use('/', home)
-router.use('/todos', todos)
-router.use('/users', users)
+router.use("/", home)
+router.use("/todos", todos)
 
 // Export route module
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,18 +1,53 @@
 // Import express router
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const Todo = require('../../models/todo')
+const Todo = require("../../models/todo")
+const User = require("../../models/user")
 
-// Define router -> Home Page
-
-router.get('/', (req, res) => {
+// Home Page (Logged in)
+router.get("/", (req, res) => {
   Todo.find() // Tell Todo data model to find data in MongoDB through mongoose. This equals to (SELECT * FROM "todos") in SQL
     .lean() // Transfer the mongoose object into clean Javascript array
-    .sort({ _id: 'asc' })
-    .then(todos => res.render('index', { todos })) // Then, pass the data to index partial template
-    .catch(error => console.log(error)) // Print the error message
+    .sort({ _id: "asc" })
+    .then((todos) => res.render("index", { todos })) // Then, pass the data to index partial template
+    .catch((error) => console.log(error)) // Print the error message
 })
 
+// Login Page
+router.get("/login", (req, res) => {
+  res.render("login")
+})
+
+router.post("/login", (req, res) => {
+  console.log("POST /login")
+})
+
+// Register Page
+router.get("/register", (req, res) => {
+  res.render("register")
+})
+
+router.post("/register", (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      console.log("User already exist")
+      res.render("register", {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      User.create({
+        name,
+        email,
+        password
+      })
+      res.redirect("./login")
+    }
+  })
+})
 
 // Export route module
 module.exports = router
