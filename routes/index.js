a24292b59dedec014cd1bfa1c2494c06d0788a6b
Richard Widjaya <ricwidjaya@gmail.com>
2021-12-17 21:02:02 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
 // Import express to use Router
 const express = require("express")
 const router = express.Router()
+const { authenticator } = require("../middleware/auth")
 
 // Import routers
 const home = require("./modules/home")
 const todos = require("./modules/todos")
+const users = require("./modules/users")
 
 // Define route module (index -> Main Router)
+router.use("/todos", authenticator, todos)
+router.use("/users", users)
 // Landing page
-router.use("/", home)
-router.use("/todos", todos)
+router.use("/", authenticator, home)
 
 // Export route module
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,8 +2,6 @@
 const express = require("express")
 const router = express.Router()
 const Todo = require("../../models/todo")
-const User = require("../../models/user")
-const passport = require("passport")
 
 // Home Page (Logged in)
 router.get("/", (req, res) => {
@@ -12,53 +10,6 @@ router.get("/", (req, res) => {
     .sort({ _id: "asc" })
     .then((todos) => res.render("index", { todos })) // Then, pass the data to index partial template
     .catch((error) => console.log(error)) // Print the error message
-})
-
-// Login Page
-router.get("/login", (req, res) => {
-  res.render("login")
-})
-
-// Use passport middleware instead of express one
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login"
-  })
-)
-
-// Log out route
-router.get("/logout", (req, res) => {
-  req.logout()
-  res.redirect("/login")
-})
-
-// Register Page
-router.get("/register", (req, res) => {
-  res.render("register")
-})
-
-router.post("/register", (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ email }).then((user) => {
-    if (user) {
-      console.log("User already exist")
-      res.render("register", {
-        name,
-        email,
-        password,
-        confirmPassword
-      })
-    } else {
-      User.create({
-        name,
-        email,
-        password
-      })
-      res.redirect("./login")
-    }
-  })
 })
 
 // Export route module
