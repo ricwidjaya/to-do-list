802c0c3f2ee7d76b3b46909833072041933c944b
Richard Widjaya <ricwidjaya@gmail.com>
2021-12-21 20:19:31 +0800

    modify seeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,10 +1,43 @@
-const db = require('../../config/mongoose')
-const Todo = require('../todo') // 載入 todo model
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
 
-db.once('open', () => {
-  // Create seed data
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: 'name-' + i })
-  }
-  console.log('done')
+const db = require("../../config/mongoose")
+const Todo = require("../todo") // 載入 todo model
+const User = require("../user")
+const bcrypt = require("bcryptjs")
+
+// Seed user
+const SEED_USER = {
+  name: "root",
+  email: "root@example.com",
+  password: "12345678"
+}
+
+db.once("open", () => {
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash
+      })
+    )
+    .then((user) => {
+      const userId = user._id
+      return Promise.all(
+        Array.from({ length: 10 }, (_, i) =>
+          Todo.create({
+            name: `name-${i}`,
+            userId
+          })
+        )
+      )
+    })
+    .then(() => {
+      console.log("Seed Generated!")
+      process.exit()
+    })
 })
